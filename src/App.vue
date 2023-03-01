30b7ee41b2635c8dda1ccc9e0d2e567cae991bf9
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-03-01 15:04:43 +0100

    Added decks from API

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,22 @@
-<script setup></script>
+<script>
+import { store } from "./assets/data/store";
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+
+  created() {
+    axios.get(store.endpoint).then((response) => {
+      store.decks = response.data;
+      console.log(store.decks);
+    });
+  },
+};
+</script>
 
 <template></template>
 
