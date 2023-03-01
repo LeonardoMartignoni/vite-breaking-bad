a5d68c209c77ac0e10e038a21954772153dfad6e
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-03-01 16:53:27 +0100

    Added spinner icon on loading

--- src/components/DeckLoading.vue
+++ src/components/DeckLoading.vue
@@ -1,7 +1,11 @@
 <script></script>
 <template>
-  <div class="loading_page text-uppercase">
-    <span>Decks Loading</span>
+  <div class="loading_page">
+    <span class="fs-4 fst-italic">Deck loading, please wait...</span>
+    <font-awesome-icon
+      icon="fa-solid fa-spinner"
+      class="mt-3 fa-3x fa-spin-pulse"
+    />
   </div>
 </template>
 <style lang="scss" scoped>
@@ -13,6 +17,7 @@
   right: 0;
   left: 0;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   background-color: rgba($color: #000000, $alpha: 0.4);
--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import { createApp } from "vue";
+
+// Import Bootstrap
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+/* Import FontAwesome */
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+
+/* Add icons to the library */
+library.add(faSpinner);
+
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
