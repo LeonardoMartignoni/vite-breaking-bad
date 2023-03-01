7a9ef46ea0894534cd2c84d7318a12cce42b94bc
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-03-01 16:11:32 +0100

    Added cards name and archetype

--- src/components/DeckCards.vue
+++ src/components/DeckCards.vue
@@ -1,17 +1,33 @@
 <script>
 export default {
   props: {
-    card: Array,
+    card: Object,
   },
 };
 </script>
 <template>
   <div class="card_content p-2">
-    <img
-      :src="card.card_images[0].image_url"
-      alt="YuGiOh Card"
-      class="img-fluid"
-    />
+    <div class="card_infos text-center">
+      <img
+        :src="card.card_images[0].image_url"
+        alt="YuGiOh Card"
+        class="img-fluid"
+      />
+      <span class="card_name text-uppercase fw-bold">{{ card.name }}</span>
+      <span class="card_archetype">{{ card.archetype }}</span>
+    </div>
   </div>
 </template>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.card_infos {
+  background-color: coral;
+  .card_name {
+    color: white;
+    display: inline-block;
+    height: 3rem;
+  }
+  .card_archetype {
+    display: block;
+  }
+}
+</style>
